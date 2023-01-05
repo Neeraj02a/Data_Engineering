843c2bc2cac2a176348979b58da1709495930d5f
Neeraj Kumar <neeraj.kumar02a@gmail.com>
2023-01-05 13:55:32 +0530

    slicing of list

---
+++ Python/slicing_of_list.py
@@ -0,0 +1,22 @@
+if __name__ =='__main__':
+
+    list= [1,2,3,4,5,6,7,8,9,10]
+
+    # slicing of list from 3-8
+    print("slicing of list from 3-8 :",list[3:8])
+
+    # slicing of list from 5 - till end
+    print("slicing of list from 5 - till end :",list[5:])
+
+    # slicing of list
+    print("slicing of the list using this way :",list[:])
+
+    # reversing a list using slicing method
+    print("reversing a list using slicing method : ", list[::-1])
+
+    # access last element of a list
+    print("access last element of a list :", list[-1])
+
+    # accessing the element from reverse order
+    print("accessing the element from reverse order :", list[4:-1])
+
