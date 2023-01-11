f406614511c284ab4c803a504a5d635a63335aeb
Neeraj Kumar <neeraj.kumar02a@gmail.com>
2023-01-11 13:37:09 +0530

    connecting python to SQL DB

--- Python/connect_to_SQL_DB.py
+++ Python/connect_to_SQL_DB.py
@@ -9,3 +9,22 @@ for i in cursor:
 	print(i)
 
 connection.close()
+
+#=============================================================================================================================================================================
+print("#============================================================ show all the table in the DB ===========================================================================")
+
+import pymysql
+import pandas as pd
+
+try:
+    myconn = pymysql.connect(user='root',password='Welcome@123',host='localhost', database='neeraj_db')
+    cursor = myconn.cursor()
+    query = "show tables;"
+    result_dataFrame = pd.read_sql(query, myconn)
+    print(result_dataFrame)
+    myconn.close() #close the connection
+except Exception as e:
+
+
+    myconn.close()
+    print(str(e))
